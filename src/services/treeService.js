c575eb92913a19582b3019dab3518e96c86597e8
SajedaHussain <sajedataqi22@gmail.com>
2026-01-05 21:36:14 +0300

    finish the treeService

--- src/services/treeService.js
+++ src/services/treeService.js
@@ -1,63 +1,59 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`;//so we can use the same url to other controllers if there is
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tree`;
 
 const index = async () => {
     try {
-        const response = await axios.get(BASE_URL)//axios can do the return res.json() already so no need to writ it
-        return response.data.pets //response.data is the json {pets:[] that are in the api} 
-
+        const response = await axios.get(BASE_URL);
+        return response.data.trees; 
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-    catch (error) {
+};
 
-        console.log(error)
-    }
-}
 
-const show = async (id) => {//GET THE ID FROM THE COMPONNT
+const show = async (id) => {
     try {
-        const response = await axios.get(`${BASE_URL}/${id}`)
-        return response.data.pet//the response get us one pet check the one pet in post man
+        const response = await axios.get(`${BASE_URL}/${id}`);
+        return response.data.tree; 
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
 
-    }
-    catch (error) {
-        console.log(error)
-    }
-}
 
 const create = async (formData) => {
     try {
-        const response = await axios.post(BASE_URL, formData)
-        return response.data.pet
+        const response = await axios.post(BASE_URL, formData);
+        return response.data.tree;
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-    catch (error) {
-        console.log(error)
-    }
-}
+};
 
-const update = async (petId, formData) => {
+
+const update = async (treeId, formData) => {
     try {
-        const response = await axios.put(`${BASE_URL}/${petId}`, formData)
-        return response.data.pet
+        const response = await axios.put(`${BASE_URL}/${treeId}`, formData);
+        return response.data.tree;
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-    catch (error) {
-        console.log(error)
-    }
-}
+};
 
-const deleteOne = async (petId) => {
+const deleteOne = async (treeId) => {
     try {
-        const response = await axios.delete(`${BASE_URL}/${petId}`)
-        return response.data.pet
-
+        const response = await axios.delete(`${BASE_URL}/${treeId}`);
+        return response.data; 
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-
-    catch (error) {
-        console.log(error)
-    }
-
-
-}
+};
 
 export {
     index,
@@ -65,5 +61,4 @@ export {
     create,
     update,
     deleteOne,
-
-}
+};
