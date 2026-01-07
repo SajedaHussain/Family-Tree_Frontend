8271f1d5068e15172889344a6a608e69c45da80c
SajedaHussain <sajedataqi22@gmail.com>
2026-01-07 18:18:16 +0300

    edit the add new

--- src/services/memberService.js
+++ src/services/memberService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/member`;
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/members`;
 
 const index = async (tree_id) => {
     try {
