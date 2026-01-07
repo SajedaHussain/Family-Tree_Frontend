5c8dd79e73ed4410f981836db120df3014996a7b
SajedaHussain <sajedataqi22@gmail.com>
2026-01-07 23:55:21 +0300

    edit list

--- src/components/Tree/TreeList.jsx
+++ src/components/Tree/TreeList.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Tree from 'react-d3-tree';
-//rafce for arrow function
-const TreeList = ({trees}) => {
 
+const TreeList = ({ trees }) => {
   return (
     <div>
       <h1>Tree List</h1>
-      {!trees.length ? <div>No Trees found </div> : 
+      {!trees.length ? (
+        <div>No Trees found</div>
+      ) : (
         <ul>
-        {
-            trees.map((oneTree)=> (
-                <li key={oneTree._id}>
-                    <Link to={`/trees/${oneTree._id}`}>{oneTree.lastName} Family</Link>
-                </li>
-            ))
-        }
-      </ul>}
+          {trees.map((familyTree) => (
+            <li key={familyTree._id}>
+              <Link to={`/trees/${familyTree._id}`}>
+                {familyTree.lastName} Family
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
 
 export default TreeList
+
