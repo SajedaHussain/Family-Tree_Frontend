78c0292c57465d69be153ee82f217062a55a9804
Hanan-k20 <98twinshanan@gmail.com>
2026-01-08 23:48:35 +0300

    no thing

--- src/services/memberService.js
+++ src/services/memberService.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/members`;
 
-
 // GET all members OR members by tree
 const index = async (tree_id) => {
   try {
