76ef6e7ed20c360cb8357f374aa51a36475b0eae
SajedaHussain <sajedataqi22@gmail.com>
2026-01-09 13:18:03 +0300

    import Css

--- src/components/Member/MemberList/MemberList.jsx
+++ src/components/Member/MemberList/MemberList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import './MemberList.css'
 
 const MemberList = ({ members }) => {
   const { treeId } = useParams();
