0acbb2892fe6b92514f54398762e8ebd29e10150
SajedaHussain <sajedataqi22@gmail.com>
2026-01-09 13:22:27 +0300

    add Css file for sign in and import it

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,9 +1,8 @@
 import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import { signIn } from '../../services/authService';
-
 import { UserContext } from '../../contexts/UserContext';
+import './SignInForm.css'
 
 const SignInForm = () => {
   const navigate = useNavigate();
